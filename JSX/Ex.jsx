92deedbffe0603445ab0960fe04d950952b0857c
haaaaa722 <hyt.7vanilla.twns@gmail.com>
2025-05-24 03:16:55 +0900

    try react

---
+++ JSX/Ex.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Ex extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {menu:open};
+    }
+    render() { 
+        return(
+            <div>
+                <button className="btn_open" onClick={()=>setState({menu:open})}><FontAwesomeIcon icon="fa-solid fa-bars"/></button>
+                <button className="btn_close" onClick={()=>setState({menu:closed})}><FontAwesomeIcon icon="fa-solid fa-xmark"/></button>
+                <li>
+                    <ul><a href="/index.html">home</a></ul>
+                    <li><a href="/develop.html">develop</a></li>
+                    <li><a href="/photo.html">photo</a></li>
+                    <li><a href="/ex.html">experiment</a></li>
+                </li>
+            </div>
+        )
+    }
+}
+
+export default Ex;
