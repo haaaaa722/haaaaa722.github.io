458d2ba87ece655788050684a49402e95991487c
haaaaa722 <hyt.7vanilla.twns@gmail.com>
2025-05-27 03:13:51 +0900

    challenge react

--- JS/Ex.jsx
+++ JS/Ex.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
 
-class Ex extends Component {
+const element = <FontAwesomeIcon icon={byPrefixAndName.fas['house']} />
+
+class Ex extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {menu:open};
+        this.state = {menu:"opne"};
     }
     render() { 
         return(
             <div>
-                <button className="btn_open" onClick={()=>setState({menu:open})}><FontAwesomeIcon icon="fa-solid fa-bars"/></button>
-                <button className="btn_close" onClick={()=>setState({menu:closed})}><FontAwesomeIcon icon="fa-solid fa-xmark"/></button>
+                <button className="btn_open" onClick={()=>this.setState({menu:"open"})}><FontAwesomeIcon icon="fa-solid fa-bars"/></button>
+                <button className="btn_close" onClick={()=>this.setState({menu:"closed"})}><FontAwesomeIcon icon="fa-solid fa-xmark"/></button>
                 <li>
                     <ul><a href="/index.html">home</a></ul>
                     <li><a href="/develop.html">develop</a></li>
@@ -18,8 +22,8 @@ class Ex extends Component {
                     <li><a href="/ex.html">experiment</a></li>
                 </li>
             </div>
-        )
+        );
     }
 }
 
-ReactDOM.render(<Ex/>,document.getElementById("hamburger"))
+ReactDOM.render(<Ex/>,document.getElementById("hamburger"));
