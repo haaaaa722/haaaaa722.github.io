eeb23d93a4c57d8fe75d77902918746b15f7bd19
haaaaa722 <hyt.7vanilla.twns@gmail.com>
2025-06-12 17:21:03 +0900

    create category hover

--- src/pages/Photos/Photos.js
+++ src/pages/Photos/Photos.js
@@ -9,13 +9,13 @@ function Photos() {
                 <nav>
                     <p>category</p>
                     <ul className="category">
-                        <li><button>Animals</button></li>
-                        <li><button>Architecture</button></li>
-                        <li><button>Attractions</button></li>
-                        <li><button>Events</button></li>
-                        <li><button>Flowers</button></li>
-                        <li><button>Interios</button></li>
-                        <li><button>Landscape</button></li>
+                        <li><button className="category-btn">Animals</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Architecture</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Attractions</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Events</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Flowers</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Interios</button> <button className="sub-category-open">+</button></li>
+                        <li><button className="category-btn">Landscape</button> <button className="sub-category-open">+</button></li>
                     </ul>
                 </nav>
             </div>
