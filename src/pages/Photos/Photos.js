de90bc20362341a369a1934834fbf5194cd4166d
haaaaa722 <hyt.7vanilla.twns@gmail.com>
2025-06-12 13:56:02 +0900

    create category

--- src/pages/Photos/Photos.js
+++ src/pages/Photos/Photos.js
@@ -1,12 +1,26 @@
 import "./Photos.css";
-import photos from "./photosData";
+import photosData from "./photosData";
 
-const Photos = ()=>{
-    return(
+function Photos() {
+    return (
         <div className='Photos'>
-            <h1>Photos</h1>
+            <div className="category-wrapper">
+                <h1>Photos</h1>
+                <nav>
+                    <p>category</p>
+                    <ul className="category">
+                        <li><button>Animals</button></li>
+                        <li><button>Architecture</button></li>
+                        <li><button>Attractions</button></li>
+                        <li><button>Events</button></li>
+                        <li><button>Flowers</button></li>
+                        <li><button>Interios</button></li>
+                        <li><button>Landscape</button></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
-};
+}
 
 export default Photos;
