e4b22aa95fbbbd4997ccc3662e93327f996ce656
haaaaa722 <hyt.7vanilla.twns@gmail.com>
2025-07-20 22:27:09 +0900

    add profile link

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,7 @@
 import icon from "../../src/images/IMG_9381.JPG";
+import xIcon from "../../src/images/x-icon.png";
+import instagramIcon from "../../src/images/instagram-icon.png";
+import githubIcon from "../../src/images/github-icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import "./Profile.css"
@@ -6,25 +9,45 @@ import "./Profile.css"
 const Profile = ()=>{
     return(
         <div className="Profile">
-            <div className="profile-wrapper">
-                <h1 className="profile-title">Profile</h1>
-                <h2>はっ！</h2>
-                <p>
-                    2003年生まれ<br/>
-                    ただの大学生<br/>
-                    エンジニアの園児<br/><br/>
-                    好きなこと<br/>
-                    ・写真を撮ること<br/>
-                    ・ゲーム
-                </p>
-                <br/>
-                <a href="https://x.com/haaaaa722" target="_brank">X <FontAwesomeIcon icon={faArrowUpRightFromSquare}/></a><br/>
-                <a href="https://instagram.com/hyt_o_olll" target="_brank">Instagram <FontAwesomeIcon icon={faArrowUpRightFromSquare}/></a><br/>
-                <a href="https://github.com/haaaaa722" target="_brank">GitHub <FontAwesomeIcon icon={faArrowUpRightFromSquare}/></a>
+            <div className="profile-main">
+                <div className="profile-main-left">
+                    <h1 className="profile-title">Profile</h1>
+                    <div className="profile-image-wrapper">
+                        <img src={icon} alt="" className="profile-image"></img>
+                    </div>
+                </div>
+
+                <div className="profile-wrapper">
+                    <div className="profile-text">
+                        <h2>はっ！</h2>
+                        <p>
+                            2003年生まれ<br/>
+                            ただの大学生<br/>
+                            エンジニアの園児<br/><br/>
+                            好きなこと<br/>
+                            ・写真を撮ること<br/>
+                            ・ゲーム
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div className="profile-image-wrapper">
-                <img src={icon} alt="" className="profile-image"></img>
+
+            <div className="profile-links-wrapper">
+                <h1>Links</h1>
+
+                <div className="profile-links">
+                    <div>
+                        <a href="https://x.com/haaaaa722" target="_blank" rel="noopener noreferrer"><img src={xIcon} alt="X Icon" className="profile-link-icon"/></a>
+                    </div>
+                    <div>
+                        <a href="https://www.instagram.com/pics._.hyt" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram Icon" className="profile-link-icon"/></a>
+                    </div>
+                    <div>
+                        <a href="https://github.com/haaaaa722" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="GitHub Icon" className="profile-link-icon"/></a>
+                    </div>
+                </div>
             </div>
+
             <div className="contact-wrapper">
                 <h1>Contact</h1>
                 <p>お問い合わせは、下記の各種SNSのDMにお願いします</p>
